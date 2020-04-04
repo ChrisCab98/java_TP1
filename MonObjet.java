ceade53ab7cf56d44df8ed8e7f1f13d80daa7650
christopher.cabantous <christopher.cabantous@devisubox.com>
2020-04-04 09:52:15 +0200

    2eme code en Java

--- MonObjet.java
+++ MonObjet.java
@@ -1,6 +1,23 @@
 public class MonObjet
 {
+    private long valeur;
+
+    public MonObjet()
+    {
+        System.out.println("Constructeur");
+        valeur = 2;
+    }
+
+    public void affichePuisModifieValeur(long nouvelleValeur)
+    {
+        System.out.println("Affiche de la valeur : " + valeur);
+        valeur = nouvelleValeur;
+    }
+
     public static void main(String arguments[]) {
-        System.out.println("L'algo c'est rigolo");
+        MonObjet objet = new MonObjet();
+        objet.affichePuisModifieValeur(10);
+        System.out.println(("L'algo c'est rigolo ?"));
+        objet.affichePuisModifieValeur(20);
     }
 }
