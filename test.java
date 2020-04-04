d83644613190d27402d59aed9689fef3289664f2
christopher.cabantous <christopher.cabantous@devisubox.com>
2020-04-04 10:25:55 +0200

    Fabriquer et utiliser une classe abstraite

--- test.java
+++ test.java
@@ -20,3 +20,17 @@ class Chaine implements ObjetMesurable // Chaine se conforme a l'interface Objet
     }
 
 }
+
+abstract class BiPoint {
+    protected double x1;
+    protected double y1;
+    protected double x2;
+    protected double y2;
+
+    public BiPoint(double x1, double y1, double x2, double y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+}
