25fe7a936b09793a0e90764fe65d38e96f8b0a02
christopher.cabantous <christopher.cabantous@devisubox.com>
2020-04-04 10:30:18 +0200

    Ajout de la classe Segment

--- test.java
+++ test.java
@@ -34,3 +34,16 @@ abstract class BiPoint {
         this.y2 = y2;
     }
 }
+
+class Segment extends BiPoint implements ObjetMesurable
+{
+    public Segment(double x1, double y1, double x2, double y2)
+    {
+        super(x1,y2,x2,y2);
+    }
+
+    public  double longueur() {
+        return Math.sqrt(Math.pow(x2-x1, 2)+Math.pow((y2-y1), 2));
+    }
+
+}
